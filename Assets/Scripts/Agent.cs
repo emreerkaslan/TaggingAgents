7011038111c2e3a14e820d0e7a2eb8a6c6f42583
emreerkaslan <67108859+emreerkaslan@users.noreply.github.com>
2022-05-30 00:35:34 +0300

    Initial Tagging Setup
    Initial tagging game is setup.

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour
+{
+    public GameObject[] agents;
+    public bool agentMode;
+    public Transform goal;
+    private Component script;
+    private Rigidbody rb;
+    public Material runnerMaterial;
+    public Material taggerMaterial;
+
+    void Start()
+    {
+        runnerMaterial = Resources.Load("Materials/blue", typeof(Material)) as Material;
+        taggerMaterial = Resources.Load("Materials/red", typeof(Material)) as Material;
+        agents = GameObject.FindGameObjectsWithTag("Agent");
+        rb = this.GetComponent<Rigidbody>();
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
+        Renderer rend = GetComponent<Renderer>();
+        if (rend != null)
+        {
+            if (agentMode)
+            {
+                rend.material = taggerMaterial;
+            }
+            else
+            {
+                rend.material = runnerMaterial;
+            }
+        }
+
+        if(findClosestEnemy() != null)
+        {
+            agent.destination = findClosestEnemy().position;
+        }
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    private void FixedUpdate()
+    {
+        NavMeshAgent thisAgent = GetComponent<NavMeshAgent>();
+        if (agentMode)
+        {
+            //tagging
+            Renderer rend = GetComponent<Renderer>();
+            if (rend != null)
+            {
+                rend.material = taggerMaterial;
+            }
+            thisAgent.destination = findClosestEnemy().position;
+        }
+        else
+        {
+            //running
+            Renderer rend = GetComponent<Renderer>();
+            if (rend != null)
+            {
+                rend.material = runnerMaterial;
+            }
+            Vector3 direction = thisAgent.transform.position - findClosestEnemy().position;
+            direction.Normalize();
+            NavMeshHit hit;
+            if(!NavMesh.Raycast(thisAgent.transform.position, (Vector3)thisAgent.transform.position + (direction * 2), out hit, NavMesh.AllAreas))
+            {
+                thisAgent.destination = (Vector3)thisAgent.transform.position + (direction * 2);
+            }
+            else
+            {
+                thisAgent.destination = ReturnRandomLocationInPlayableArea(thisAgent.transform.position, 5, -1);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (agentMode && collision.gameObject.tag == "Agent" && collision.gameObject.GetComponent<Agent>() != null)
+        {
+            Agent otherAgent = collision.gameObject.GetComponent<Agent>();
+            if (!otherAgent.agentMode)
+            {
+                otherAgent.agentMode = true;
+            }
+        }
+    }
+
+    public void setAgentMode(bool mode)
+    {
+        this.agentMode = mode;
+    }
+
+    public bool getAgentMode()
+    {
+        return this.agentMode;
+    }
+
+    private Transform findClosestEnemy()
+    {
+        float closestDistance = float.MaxValue;
+        Transform tempGoal = null;
+        foreach (GameObject agent in agents)
+        {
+            Agent enemy = agent.GetComponent<Agent>();
+            if(enemy != null)
+            {
+                if (enemy.agentMode != this.agentMode)
+                {
+                    Transform location = enemy.transform;
+                    float distance = Mathf.Sqrt(Mathf.Pow(location.position.x - transform.position.x, 2f) + Mathf.Pow(location.position.z - transform.position.z, 2f));
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        tempGoal = enemy.transform;
+                    }
+                }
+            }
+        }
+        return tempGoal;
+    }
+
+    private static Vector3 ReturnRandomLocationInPlayableArea(Vector3 origin, float distance, int layermask)
+    {
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
+
+        randomDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+        
+        return navHit.position;
+    }
+
+    private bool CheckIfPointInPlayableArea(Vector3 destination)
+    {
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(destination, out hit, 1f, NavMesh.AllAreas))
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/AgentManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    public GameObject[] agents;
+
+    private void Awake()
+    {
+        int totalTaggers = 0;
+        agents = GameObject.FindGameObjectsWithTag("Agent");
+        print("started assigning");
+        while (totalTaggers < 2)
+        {
+            print("total" + totalTaggers.ToString());
+            print("started loop");
+            int rnd = Random.Range(0, agents.Length - 1);
+            print("random" + rnd.ToString());
+            Agent agent = agents[rnd].GetComponent<Agent>();
+            if (agent != null)
+            {
+                print("agent found");
+                if (agent.agentMode != true)
+                {
+                    print("created agent");
+                    agent.agentMode = true;
+                    totalTaggers = totalTaggers + 1;
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        int totalRunners = 0;
+        foreach (GameObject agent in agents)
+        {
+            Agent enemy = agent.GetComponent<Agent>();
+            if (enemy != null)
+            {
+                if (enemy.agentMode == false)
+                {
+                    totalRunners = totalRunners + 1;
+                }
+            }
+        }
+
+        if (totalRunners <= 2)
+        {
+            foreach (GameObject agent in agents)
+            {
+                Agent enemy = agent.GetComponent<Agent>();
+                if (enemy != null)
+                {
+                    enemy.agentMode = !enemy.agentMode;
+                }
+            }
+        }
+    }
+}
